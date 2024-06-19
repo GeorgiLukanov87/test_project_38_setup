ed7faf832d2721cbbbba52a58336df25d34c517a
Georgi St. Lukanov <goshiii87@gmail.com>
2024-06-19 15:32:10 +0300

    .

--- project_38_georgi/accounts/models.py
+++ project_38_georgi/accounts/models.py
@@ -1,13 +1,17 @@
+from django.core import validators
 from django.db import models
 from django.contrib.auth import models as auth_models
 
 
 class TestUser(auth_models.AbstractUser):
     username = models.CharField(
-        max_length=30
+        max_length=30,
+        validators=[
+            validators.MinLengthValidator(3, message='Username must be at least 3 characters long')
+        ]
     )
 
-    email = models.CharField(
+    email = models.EmailField(
         max_length=200,
         unique=True
     )
