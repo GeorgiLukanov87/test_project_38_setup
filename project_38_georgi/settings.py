33197c8a007aac3c0f479626d9a48a8bf6eca6db
Georgi St. Lukanov <goshiii87@gmail.com>
2024-06-25 18:40:49 +0300

    add twilio and sendgrid libraries

--- project_38_georgi/settings.py
+++ project_38_georgi/settings.py
@@ -103,3 +103,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+# settings.py
+
+TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
+
