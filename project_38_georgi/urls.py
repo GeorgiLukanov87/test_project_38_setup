ba0d9c360eebdf5291ec0e1000d5e64c6dfd9286
Georgi St. Lukanov <goshiii87@gmail.com>
2024-07-09 13:07:34 +0300

    refactor lines

--- project_38_georgi/urls.py
+++ project_38_georgi/urls.py
@@ -3,8 +3,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
-urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', include('project_38_georgi.accounts.urls')),
-
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [path('admin/', admin.site.urls),
+               path('', include('project_38_georgi.accounts.urls')),
+               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
