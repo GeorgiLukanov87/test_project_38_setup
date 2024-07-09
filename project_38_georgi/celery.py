01b7822f875949c0c453b9169d3474e71bbc6de2
Georgi St. Lukanov <goshiii87@gmail.com>
2024-07-10 01:08:38 +0300

    .

--- project_38_georgi/celery.py
+++ project_38_georgi/celery.py
@@ -1,24 +1,18 @@
-# celery.py
-
-from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "celery_demo.settings")
 
-app = Celery('myproject')
+app = Celery("project_38_georgi")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.update(
+    worker_concurrency=2,
+)
 
-# Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
 
 
 @app.task(bind=True)
 def debug_task(self):
-    print(f'Request: {self.request!r}')
+    print(f"Request: {self.request!r}")
