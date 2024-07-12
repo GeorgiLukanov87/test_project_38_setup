0f1c3a228356db96b513b9cc5c40db692d840bcc
Georgi St. Lukanov <goshiii87@gmail.com>
2024-07-12 17:51:38 +0300

    .

--- project_38_georgi/accounts/models.py
+++ project_38_georgi/accounts/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth import models as auth_models
 
 
 class TestUser(auth_models.AbstractUser):
+    id = models.BigAutoField(
+        primary_key=True,
+    )
+
     username = models.CharField(
         max_length=30,
         validators=[
@@ -14,4 +18,16 @@ class TestUser(auth_models.AbstractUser):
     email = models.EmailField(
         max_length=200,
         unique=True
+    )
+
+    created_date = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    last_modified_by = models.CharField(
+        max_length=50
+    )
+
+    last_modified_date = models.DateTimeField(
+        auto_now=True,
     )
