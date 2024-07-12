f3c07d370e0a2d37f6524482d28cec961112adc6
Georgi St. Lukanov <goshiii87@gmail.com>
2024-07-12 18:18:45 +0300

    update pipeline for postgres db

--- project_38_georgi/settings.py
+++ project_38_georgi/settings.py
@@ -64,12 +64,12 @@ WSGI_APPLICATION = 'project_38_georgi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 DATABASES = {
     "default": {
