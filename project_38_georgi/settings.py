2231821aab7cfc63f02d1e87e5b4b307cff0b25c
Georgi St. Lukanov <goshiii87@gmail.com>
2024-07-12 18:17:36 +0300

    update pipeline for postgres db

--- project_38_georgi/settings.py
+++ project_38_georgi/settings.py
@@ -71,16 +71,16 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv('POSTGRES_DB'),
-#         "USER": os.getenv('POSTGRES_USER'),
-#         "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv('POSTGRES_DB'),
+        "USER": os.getenv('POSTGRES_USER'),
+        "PASSWORD": os.getenv('POSTGRES_PASSWORD'),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
