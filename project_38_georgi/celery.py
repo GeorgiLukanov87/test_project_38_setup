9ac718106311399032df5c3611640f769c1c0889
Georgi St. Lukanov <goshiii87@gmail.com>
2024-07-15 13:24:24 +0300

    .

--- project_38_georgi/celery.py
+++ project_38_georgi/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "celery_demo.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_38_georgi.settings")
 
 app = Celery("project_38_georgi")
 app.config_from_object("django.conf:settings", namespace="CELERY")
